7de5b408c60cf7e7a3086a3e7357188f045bc579
xenxynon <flynryder427@gmail.com>
2022-10-16 15:45:34 +0000

    Revert "Launcher3: Add Google Lens scan button to the recent apps"
    This reverts commit a28b8496a93c3efa887494f62f27200694963eb0.

--- quickstep/src/com/android/quickstep/TaskOverlayFactory.java
+++ quickstep/src/com/android/quickstep/TaskOverlayFactory.java
@@ -363,15 +363,6 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
             public void onClearAllTasksRequested() {
                 endLiveTileMode(TaskOverlay.this::clearAllTasks);
             }
-
-            @Override
-            public void onLens() {
-                if (mIsAllowedByPolicy) {
-                    endLiveTileMode(() -> mImageApi.startLensActivity());
-                } else {
-                    showBlockedByPolicyMessage();
-                }
-            }
         }
     }
 
@@ -387,7 +378,5 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
         void onSplit();
 
         void onClearAllTasksRequested();
-
-        void onLens();
     }
 }
